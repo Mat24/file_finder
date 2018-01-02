d5aea9b09435852b46ce3890a4af1bb1886fbdd0
sebastian-aya-globant <sebastian.aya@globant.com>
2018-01-02 15:54:35 -0500

    refactor finder pattern matching

--- lib/file_finder.ex
+++ lib/file_finder.ex
@@ -17,15 +17,13 @@ defmodule FileFinder do
   
     def finder([], _actual_path, _exp, acc), do: acc
   
-    def finder({:ok, files}, actual_path, exp, acc), do: finder(files, actual_path, exp, acc)
-  
     def finder([head | tail], actual_path, exp, acc) do
       IO.puts "Processing... #{Path.join(actual_path,head)}"
       case File.dir? head do
         true ->
               new_path = Path.join(actual_path,head)
-              subfinder = finder([path: new_path, filename: exp], []) 
-              finder(tail, actual_path, exp, [subfinder | acc])
+              subfinder = Task.async(fn -> finder([path: new_path, filename: exp], []) end) 
+              finder(tail, actual_path, exp, [Task.await(subfinder) | acc])
         false when head == exp -> finder(tail, actual_path, exp, ["#{actual_path}/#{exp}"|acc])
         false -> finder(tail, actual_path, exp, acc)
       end
